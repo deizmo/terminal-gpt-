d03f909c902ccd79c3ce8c74fac8a3353e8f5e30
deizmo <68545254+deizmo@users.noreply.github.com>
2023-10-14 20:21:54 +0200

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -4,19 +4,19 @@ g4f.logging = True # enable logging
 g4f.check_version = False # Disable automatic version checking
 print(g4f.version) # check version
 print(g4f.Provider.Ails.params)  # supported args
-jews = sys.argv[1]
+args = sys.argv[1]
 i = 0
 # Automatic selection of provider
 while True:
     if i == 0:
         pass
     else:
-        jews = str(input(':'))
+        args = str(input(': '))
 
     response = g4f.ChatCompletion.create(
         model=g4f.models.gpt_4,
-        messages=[{"role": "user", "content": jews}],
+        messages=[{"role": "user", "content": args}],
     )
-    jews = " "
+    args = " "
     i = 1
     print(response)
