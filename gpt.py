6b3e5bbcf91bc5b143f866fcf7182c33dbac2621
deizmo <68545254+deizmo@users.noreply.github.com>
2023-10-18 12:48:27 +0200

    Add files via upload
    Added history to chat

--- gpt.py
+++ gpt.py
@@ -4,41 +4,52 @@ g4f.logging = True # enable logging
 g4f.check_version = False # Disable automatic version checking
 print(g4f.version) # check version
 print(g4f.Provider.Ails.params)  # supported args
-jews=''
-for arg in sys.argv[1:]:
-    arg = str(arg)
-    jews = jews +" "+ arg
-print(jews[0:3])
-argument = jews[0:3]
-i = 0
-if jews == '':
-    print('no argument')
-# Automatic selection of provider
-while True:
-    if i == 0:
-        pass
-    else:
+
+
+def main_without_termianlArgs():
+    while True:
+        history.append({"role": "user", "content": jews})
+
+        response = g4f.ChatCompletion.create(
+            model=g4f.models.gpt_4,
+            messages=history,
+        )
+        i = 1
+        print(response)
+        print(history)
+
+        if argument == ' -1':
+            print(' \n Session has ended ')
+            break
+        else:
+            pass
+
+        jews = " "
+
+def main_withArgs():
+    while True:
         jews = str(input(':'))
-    response = g4f.ChatCompletion.create(
-        model=g4f.models.gpt_4,
-        messages=[{"role": "user", "content": jews}],
-    )
-    jews = " "
-    i = 1
-    print(response)
+        history.append({"role": "user", "content": jews})
+        response = g4f.ChatCompletion.create(
+            model=g4f.models.gpt_4,
+            messages=history,
+        )
+        i = 1
+        print(response)
 
-    if argument == ' -1':
-        print(' \n Session has ended ')
-        break
-    else:
-        pass
+        jews = " "
 
 
-def gpt(jews):
-    jews = str(jews)
-    response = g4f.ChatCompletion.create(
-    model=g4f.models.gpt_4,
-    messages=[{"role": "user", "content": jews}],
-)
-def gpt_answer():
-    pass
+history = []
+argument = ""
+try:
+    for arg in sys.argv[1:]:
+        arg = str(arg)
+        jews = jews +" "+ arg
+    #print(jews[0:3])
+    argument = jews[0:3]
+    i = 0
+    
+    main_without_termianlArgs()
+except:
+    main_withArgs()
